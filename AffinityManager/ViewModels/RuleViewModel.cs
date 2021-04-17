b12bddbcadb98a4552240013c607d7e42af6b93b
Tomasz Rewak <tomaszrewak@gmail.com>
2021-04-17 23:55:34 +0200

    RuleViewModel

---
+++ AffinityManager/ViewModels/RuleViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Privatest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AffinityManager.ViewModels
+{
+	internal sealed class RuleViewModel : BaseViewModel
+	{
+		[BackingField] private string _name;
+		public string Name
+		{
+			get => _name;
+			set => Set(ref _name, value);
+		}
+	}
+}
