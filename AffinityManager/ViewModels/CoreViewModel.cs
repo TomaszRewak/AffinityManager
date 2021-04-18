f29348decc29bc26e1bbed668fcc2564e567bcd5
Tomasz Rewak <tomaszrewak@gmail.com>
2021-04-18 23:45:59 +0200

    Small cleanup.

--- AffinityManager/ViewModels/CoreViewModel.cs
+++ AffinityManager/ViewModels/CoreViewModel.cs
@@ -1,10 +1,5 @@
 ﻿using AffinityManager.Common;
 using Privatest;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace AffinityManager.ViewModels
