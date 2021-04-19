b3e904ce8386e79efd8e599c5b0b97fd076da75e
Tomasz Rewak <tomaszrewak@gmail.com>
2021-04-19 23:20:38 +0200

    Getting number of active processes.

--- AffinityManager/ViewModels/ManagerViewModel.cs
+++ AffinityManager/ViewModels/ManagerViewModel.cs
@@ -1,15 +1,27 @@
 ﻿using Privatest;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Threading;
 
 namespace AffinityManager.ViewModels
 {
 	internal sealed class ManagerViewModel : BaseViewModel
 	{
+		private readonly DispatcherTimer _timer;
+
 		[BackingField] private IReadOnlyCollection<RuleViewModel> _rules = new List<RuleViewModel>();
 		public IReadOnlyCollection<RuleViewModel> Rules
 		{
 			get => _rules;
 			set => Set(ref _rules, value);
+		}
+
+		[BackingField] private int _numberOfProcesses;
+		public int NumberOfProcesses
+		{
+			get => _numberOfProcesses;
+			set => Set(ref _numberOfProcesses, value);
 		}
 
 		public ManagerViewModel()
@@ -20,6 +32,14 @@ namespace AffinityManager.ViewModels
 				new RuleViewModel(),
 				new RuleViewModel()
 			};
+
+			_timer = new DispatcherTimer(TimeSpan.FromSeconds(10), DispatcherPriority.Background, (e, a) => UpdateProcesses(), Dispatcher.CurrentDispatcher);
+			_timer.Start();
+		}
+
+		private void UpdateProcesses()
+		{
+			NumberOfProcesses = Process.GetProcesses().Length;
 		}
 	}
 }
