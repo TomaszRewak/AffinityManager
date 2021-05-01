399fb50e4ddf1460216bf89b780566a8f0f24ecb
Tomasz Rewak <tomaszrewak@gmail.com>
2021-05-01 21:27:39 +0200

    Custom drag panel

---
+++ AffinityManager/Controls/DragAndDropPanel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AffinityManager.Controls
+{
+	internal sealed class DragAndDropPanel : Panel
+	{
+		protected override Size MeasureOverride(Size availableSize)
+        {
+            var rowSizeLimit = new Size(availableSize.Width, 0);
+            var size = new Size();
+
+            foreach (UIElement child in Children)
+            {
+                child.Measure(rowSizeLimit);
+
+                size = new Size(
+                    Math.Max(size.Width, child.DesiredSize.Width),
+                    size.Height + child.DesiredSize.Height);
+            }
+
+            return size;
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            var top = 0;
+
+            foreach (UIElement child in Children)
+            {
+                var rect = new Rect(
+                    0,
+                    top,
+                    child.DesiredSize.Width,
+                    child.DesiredSize.Height);
+
+                child.Arrange(rect);
+            }
+
+            return DesiredSize;
+        }
+    }
+}
